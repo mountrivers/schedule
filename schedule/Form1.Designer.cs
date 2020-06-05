0d61e4a24c0e0b28f1c90076edbaa8639d27e3d4
mountrivers <36880919+mountrivers@users.noreply.github.com>
2020-06-05 22:11:41 +0900

    Update Form1.Designer.cs

--- schedule/Form1.Designer.cs
+++ schedule/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.AddListButton = new System.Windows.Forms.Button();
+            this.date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.title = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.date,
+            this.title});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 1);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(242, 449);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.UseWaitCursor = true;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // AddListButton
+            // 
+            this.AddListButton.Location = new System.Drawing.Point(646, 100);
+            this.AddListButton.Name = "AddListButton";
+            this.AddListButton.Size = new System.Drawing.Size(75, 23);
+            this.AddListButton.TabIndex = 1;
+            this.AddListButton.Text = "add";
+            this.AddListButton.UseVisualStyleBackColor = true;
+            this.AddListButton.Click += new System.EventHandler(this.AddListButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(360, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(751, 148);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.AddListButton);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button AddListButton;
+        private System.Windows.Forms.ColumnHeader date;
+        private System.Windows.Forms.ColumnHeader title;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
