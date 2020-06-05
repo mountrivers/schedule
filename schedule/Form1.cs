b03d42c50fd1c1f8c23de13e7a9315da31cd0473
mountrivers <36880919+mountrivers@users.noreply.github.com>
2020-06-05 22:12:22 +0900

    see des
    1) 리스트뷰 아이템 추가 버튼
    2) 리스트뷰 클릭 리스너 -> 텍스트 박스 내용 바뀌게 구현

--- schedule/Form1.cs
+++ schedule/Form1.cs
@@ -12,9 +12,24 @@ namespace schedule
 {
     public partial class Form1 : Form
     {
+        int tCounter = 100;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void AddListButton_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Add(new ListViewItem(new string[] {tCounter.ToString(), "100" }));
+            tCounter++;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indexNum;
+            indexNum = listView1.FocusedItem.Index;
+            string test = listView1.Items[indexNum].SubItems[0].Text;
+            textBox1.Text = test;
         }
     }
 }
